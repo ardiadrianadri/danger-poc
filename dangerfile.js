892f23d6ec5fe8932622dcf8140ec70907c69149
Adrian Ferreres <adrian.ferreres@sngular.com>
2019-05-08 18:15:14 +0200

    First introduction to devvox3000

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,36 @@
-import {message, danger} from 'danger';
+import {message, danger, fail} from 'danger';
 
-const modifiedMD = danger.git.modified_files.join('- ');
-message('Changed Files in this PR: \n - ' + modifiedMD);
+const fails = [];
+
+function postFails() {
+    const leng = fails.length;
+
+    let failMsg;
+
+    if (leng === 0) {
+        message(`Bien hecho. Esta PR es un orgullo para ti y para toda tu raza`);
+    } else {
+        failMsg = fails.reduce((prev, current, index) => {
+            let result;
+
+            if (index === leng) {
+                result = `${prev}${current}. `;
+            } else {
+                result = `${prev}${current}, `;
+            }
+
+            return result;
+        }, `Esta PR es una vergüenza, una deshonra. Te enumero tus fallos: `);
+
+        fail(failMsg);
+    }
+}
+
+message(`
+Permite que me presente por si no nos conocemos. Soy DevVox3000; un bot del futuro que ha venido
+para asegurarse de que el codigo que subes al repositorio es digno de la raza de seres superiores
+que dominan el futuro. Mi misión; no dejarte pasar ni una ni media. Mas te vale ser bueno porque 
+de lo contrario... lo sabré
+`);
+
+postFails();
