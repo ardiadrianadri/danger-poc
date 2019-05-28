12c422a0141ad8f005e55f1dc9f2ed366047e98d
Adrian Ferreres <adrian.ferreres@sngular.com>
2019-05-28 18:18:29 +0200

    Add branch name check

--- dangerfile.js
+++ dangerfile.js
@@ -1,6 +1,12 @@
 import {message, danger, fail} from 'danger';
 
+const GITHUB_OWNER = process.env.GITHUB_OWNER;
+const GITHUB_REPO = process.env.GITHUB_REPO;
+
 const fails = [];
+const validBranchName = /^(feature|bugfix|refactor|hotfix)\/.*$/g;
+const commitsNumber = danger.github.commits.length;
+const lastCommit = danger.github.commits[commitsNumber - 1];
 
 function postFails() {
     const leng = fails.length;
@@ -20,7 +26,9 @@ function postFails() {
             }
 
             return result;
-        }, `Esta PR es una vergüenza, una deshonra. Te enumero tus fallos: `);
+        }, `
+        Esta PR es una vergüenza, una deshonra. Te enumero tus fallos: 
+        `);
 
         fail(failMsg);
     }
@@ -28,14 +36,18 @@ function postFails() {
 
 function checkReviers() {
     if (!danger.github.requested_reviewers.users.length) {
-        fails.push('Esta PR no tiene ni un revisor asignado. No pases de tus compatriotas');
+        fails.push(`
+        Esta PR no tiene ni un revisor asignado. No pases de tus compatriotas
+        `);
     }
 }
 
 
 function checkBody() {
     if (!danger.github.pr.body.length) {
-        fails.push('Tu PR no tiene descripción. Un patriota siempre explica lo quiere hacer a sus compañeros');
+        fails.push(`
+        Tu PR no tiene descripción. Un patriota siempre explica lo quiere hacer a sus compañeros
+        `);
     }
 }
 
@@ -43,8 +55,19 @@ function checkIssueRelated() {
     const validGithubIssue = /issue #[0-9]{1,5}/gm;
 
     if (!danger.github.pr.body.match(validGithubIssue)) {
-        fails.push('No has asociado a tu PR ninguna issue... serás secesionista!!');
+        fails.push(`
+        No has asociado a tu PR ninguna issue... serás secesionista!!
+        `);
     }
+}
+
+function checkBranch(branch) {
+  if(!validBranchName.test(branch.name)) {
+    fails.push(`
+    Pero bueno... ¿No te han dicho que las ramas deben empezar por feature, bugfix, refactor o hotfix? ¿Así como voy a saber como diablos quieres que
+    integre el desarrollo? No me ayudas, la verdad es que no ayudas...
+    `)
+  }
 }
 
 message(`
@@ -54,7 +77,21 @@ que dominan el futuro. Mi misión; no dejarte pasar ni una ni media. Mas te vale
 de lo contrario... lo sabré
 `);
 
-checkReviers();
-checkBody();
-checkIssueRelated();
-postFails();
+
+danger.github.api.request(
+  `/repos/${GITHUB_OWNER}/commits/${lastCommit.sha}/branches-where-head`,
+  {
+    headers: {
+      'accept': 'application/vnd.github.groot-preview+json'
+    }
+  }
+).then(response => {
+  const branches = response.data;
+  const lastBranch = branches[branches.length - 1];
+  checkBranch(lastBranch);
+  checkReviers();
+  checkBody();
+  checkIssueRelated();
+  postFails();
+})
+.catch(error => { throw error; });
