3f0018ed262318688734817fad3d0f71b7ee5459
Adrian Ferreres <adrian.ferreres@sngular.com>
2019-06-12 19:26:59 +0200

    First configuration danger

---
+++ dangerfile.js
@@ -0,0 +1,6 @@
+import { message } from 'danger';
+
+message(`
+Hola a todos soy DevVox 3000 un bot que ha resucitado de un pasado muy casposo y viene a asegurase de tu 
+codigo es digno de un desarrollador de una raza superior
+`);
