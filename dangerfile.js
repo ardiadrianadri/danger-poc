0263c6f5a0a224289912cd92604ab9e0e5bf7e87
Adrian Ferreres <adrian.ferreres@sngular.com>
2019-06-15 11:21:58 +0200

    Add danger configuration

---
+++ dangerfile.js
@@ -0,0 +1,7 @@
+import {message} from 'danger';
+
+message(`
+Hola a todos. Soy el bot dev vox 3000; un bot que viene del 
+pasado mas rancio para asegurarse de tu código es digno de una
+raza superior
+`);
